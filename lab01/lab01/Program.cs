93880840c58a71ceb30ce97b5c2eb080dc6d9c22
ccollis93 <63563783+ccollis93@users.noreply.github.com>
2022-12-29 14:33:27 -0800

    Add files via upload
    C++ lab from 2020 dealing with simple user input and output of strings

---
+++ lab01/lab01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks user for their name
+            Console.WriteLine("Please enter your name.");
+
+            //reads in the user's name
+            string usrName = Console.ReadLine();
+
+            //outputs the user's inputted name 
+            Console.WriteLine("Welcome " + usrName);
+           
+        }
+    }
+}
---
+++ lab01pt2/lab01pt2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // below are the cosole I/O for gathering the information for the story
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            string age = Console.ReadLine();
+            // this line below converts the string input to an int
+            int ageNum = Convert.ToInt32(age);
+
+            Console.WriteLine("Enter the city you live in");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter your College");
+            string coll = Console.ReadLine();
+
+            Console.WriteLine("Enter your job title");
+            string job = Console.ReadLine();
+
+            Console.WriteLine("Enter an animal species");
+            string pet = Console.ReadLine();
+
+            Console.WriteLine("Enter a name");
+            string petName = Console.ReadLine();
+
+            // the lines below print out the story, although I am having trouble getting it to display
+            Console.WriteLine("There once was a person named " + name + "who lived in " + city + " .");
+            Console.WriteLine("At the age of " + ageNum + " they went to " + coll + " .");
+            Console.WriteLine(name + "Graduated and went to work as a" + job + " .");
+            Console.WriteLine("Then, " + name + "adopted a " + pet + " named " + petName + " .");
+            Console.WriteLine("They both lived happily everafter, THE END!");
+            Console.ReadLine();
+
+           
+
+
+        }
+    }
+}
